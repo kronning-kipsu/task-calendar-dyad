8950a77325e8513d5ec147ba22e4e58633ddfe81
Kyle Ronning <kyleronning@gmail.com>
2020-12-29 18:57:21 -0600

    Implement createGoogleCalendarsInstance helper function and export GoogleCalendars at entry point for library

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
-import TasksInterface from './tasks/TasksInterface'
+import GoogleCalendars from './calendars/googleCalendar/GoogleCalendars'
 import TrelloTasks from './tasks/trello/TrelloTasks'
 
 // Exports
+export * from './calendars/googleCalendar/GoogleCalendars'
 export * from './tasks/trello/TrelloTasks'
 
-export const createTrelloTasksInstance = (apiKey: string, token: string, listId: string) => {
+export const createTrelloTasksInstance = (apiKey: string, token: string, listId: string): TrelloTasks => {
     return new TrelloTasks(apiKey, token, listId)
+}
+
+export const createGoogleCalendarsInstance = (): GoogleCalendars => {
+    return new GoogleCalendars()
 }
